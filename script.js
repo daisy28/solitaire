8333373d4de43b00eabd2de9c13c698cb6f72835
Gloria <ubahgloria16@gmail.com>
2023-07-15 15:27:35 +0100

    Set up

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("hello world");
+// type cardDeck = {
+//      cards: string[];
+//      suits: string[];
+//      ranks: string[];
+// }
+var CardDeck = /** @class */ (function () {
+    function CardDeck() {
+        this.cards = [];
+        this.suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+        this.ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+        this.generateCards();
+    }
+    CardDeck.prototype.generateCards = function () {
+        for (var _i = 0, _a = this.suits; _i < _a.length; _i++) {
+            var suit = _a[_i];
+            for (var _b = 0, _c = this.ranks; _b < _c.length; _b++) {
+                var rank = _c[_b];
+                this.cards.push("".concat(rank, " ").concat(suit));
+            }
+        }
+    };
+    CardDeck.prototype.shuffle = function () {
+        var _a;
+        for (var i = this.cards.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            return _a = [this.cards[j], this.cards[i]], this.cards[i] = _a[0], this.cards[j] = _a[1], _a;
+        }
+    };
+    return CardDeck;
+}());
+var newCard = new CardDeck();
+console.log(newCard);
+console.log(newCard.shuffle());
+console.log(newCard.shuffle());
+console.log(newCard.shuffle());
+// console.log(newCard.cards);
--- script.ts
+++ script.ts
@@ -1,2 +1,38 @@
-console.log("hello world");
+// type cardDeck = {
+//      cards: string[];
+//      suits: string[];
+//      ranks: string[];
+// }
 
+class CardDeck {
+     cards: string[];
+     suits: string[];
+     ranks: string[];
+     constructor() {
+          this.cards = [];
+          this.suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+          this.ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+          this.generateCards();
+     }
+     generateCards() {
+          for (let suit of this.suits) {
+               for (let rank of this.ranks) {
+                    this.cards.push(`${rank} ${suit}`)
+               }
+          }
+     }
+
+     shuffle() {
+          for (let i = this.cards.length - 1; i > 0; i--){
+               const j = Math.floor(Math.random() * (i + 1));
+               return [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]]
+          }
+     }
+}
+
+const newCard = new CardDeck();
+console.log(newCard);
+console.log(newCard.shuffle());
+console.log(newCard.shuffle());
+console.log(newCard.shuffle());
+// console.log(newCard.cards);
