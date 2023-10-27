19561507898792043ff3aae847df6f66333feec8
Gloria <Ubahgloria16@gmail.com>
2023-10-27 20:52:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var deltCardDiv = document.querySelector("#delt_card_div");
+thyvar deltCardDiv = document.querySelector("#delt_card_div");
 var hiddenCardDiv = document.querySelector("#hidden_card_div");
 var cardDestination = document.querySelector(".card_destination");
 var CardDeck = /** @class */ (function () {
@@ -57,23 +57,3 @@ var renderCard = function () {
     });
 };
 renderCard();
-
-
-var renderCard = function () {
-    var hiddenCardTemplate = "";
-    var deltCardTemplate = "";
-    for (var a = 0; a < 9; a++) {
-        for (var b = 1; b < a; b++) {
-            deltCardTemplate += "<div draggable=\"true\" class=\"cards\">".concat(packOfCards.deltCards[0].pop(), "</div>");
-        }
-        deltCardTemplate += "<br />";
-        deltCardDiv.innerHTML = deltCardTemplate;
-    }
-    packOfCards.hiddenCards[0].map(function (card) {
-        hiddenCardTemplate += "<div draggable=\"true\" class=\"hidden_cards\">".concat(card, "</div>");
-        hiddenCardDiv.innerHTML = hiddenCardTemplate;
-        cardDestination.append(hiddenCardDiv);
-    });
-};
-renderCard();
-
